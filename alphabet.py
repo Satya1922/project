cd8419caaa4c110b224cfefaf6cd41b59f3f9dc6
Satya1922 <52398667+Satya1922@users.noreply.github.com>
2019-07-03 10:14:23 +0530

    Update and rename vowel.py to alphabet.py

---
+++ alphabet.py
@@ -0,0 +1,6 @@
+a=str(input())
+list1=["a","e","i","o","u","b","c","d","f","g","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
+if a in list1:
+    print("Alphabet")
+else:
+    print("No")
--- vowel.py
+++
@@ -1,9 +0,0 @@
-a=str(input())
-list1=["a","e","i","o","u"]
-list2=["b","c","d","f","g","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
-if a in list1:
-    print("Vowel")
-elif a in list2:
-    print("Consonant")
-else:
-    print("Invalid")
