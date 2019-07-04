52232624b1aeb009e6e6575257e759947d02d5c9
Satya1922 <52398667+Satya1922@users.noreply.github.com>
2019-07-04 09:57:23 +0530

    Update primeinterval.py

--- primeinterval.py
+++ primeinterval.py
@@ -1,1 +1,9 @@
 lower,upper=map(int,input().split())
+for num in range(lower,upper):
+   if num>1:
+      for i in range(2,num):
+        if(num%i)==0:
+          break
+      else:
+          print(num,end=" ")
+      
