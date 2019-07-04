c6525bcdf077389b3b1043a7e009a0f1bc37b3c2
Satya1922 <52398667+Satya1922@users.noreply.github.com>
2019-07-04 09:34:18 +0530

    Update armstrong.py

--- armstrong.py
+++ armstrong.py
@@ -1,2 +1,11 @@
-num=int(input)
-temp=n
+num=int(input())
+temp=num
+sum=0
+while(num>0):
+    rem=num%10
+    sum=sum+rem*rem*rem
+    num=num//10
+if(sum==temp):
+    print("yes")
+else:
+    print("no")
